a71e9809b16e74206275e81016dfd421c5e186fc
dodevs <douglass.sousa@outlook.com.br>
2019-11-07 14:06:53 -0300

    Adicionao medicao de tempo

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include <math.h>
+#include <time.h>
 #include <stdlib.h>
 #include <pthread.h>
 
 // MATRIX
 #define MATRIX_M 10000
 #define MATRIX_N 10000
-#define MATRIX_LEN MATRIX_M * MATRIX_N
 int matrix[MATRIX_M][MATRIX_N];
 
 // MACROBLOCO
 #define MACROBLOCO_M 5000
 #define MACROBLOCO_N 5000
 #define QTD_ELEM MACROBLOCO_M * MACROBLOCO_N
-#define QTD_MB MATRIX_LEN / QTD_ELEM
 
 // CONTROLE SECAO CRITICA
 int lr, lc = 0;
@@ -29,20 +27,9 @@ pthread_mutex_t mutex;
 
 void *rotina(void *arg);
 int verificaPrimo(int num);
-int verificaPrimo2(int num);
+
 
 int verificaPrimo(int num) {
-    int resultado = 0;
-    for(int i = 2; i <= num / 2; i++) {
-        if (num % i == 0) {
-            resultado++;
-            break;
-        }
-    }
-    return resultado;
-}
-
-int verificaPrimo2(int num) {
     int d, EhPrimo;
 
     if (num <= 1 || (num != 2 && num % 2 == 0))
@@ -61,7 +48,6 @@ int verificaPrimo2(int num) {
 }
 
 void *rotina(void *arg) {
-    int qtd_elem_mn = sqrt(QTD_ELEM);
     int local_primos = 0;
 
     pthread_mutex_lock(&mutex);
@@ -78,7 +64,7 @@ void *rotina(void *arg) {
     while(local_lr < MATRIX_M && local_lc < MATRIX_N) {
         for(int i = local_lr; i < local_lr + MACROBLOCO_M; i++) {
             for(int j = local_lc; j < local_lc + MACROBLOCO_N; j++) {
-                if(verificaPrimo2(matrix[i][j])) {
+                if(verificaPrimo(matrix[i][j])) {
                     local_primos++;
                 }
             }
@@ -104,7 +90,9 @@ void *rotina(void *arg) {
 int main() {
     int i;
     srand(RANDOM_SEED);
+    clock_t ticks[2];
 
+    ticks[0] = clock();
     for(i = 0; i < MATRIX_M; i++) {
         for(int j = 0; j < MATRIX_N; j++) {
             matrix[i][j] = rand() % 29999;
@@ -118,8 +106,10 @@ int main() {
     for(i = 0; i < QTD_THREADS; i++) {
         pthread_join(tid[i], NULL);
     }
+    ticks[1] = clock();
 
     printf("Numeros primos: %d\n", primos);
+    printf("Tempo gasto: %g ms.\n", ((ticks[1] - ticks[0]) * 1000.0 / CLOCKS_PER_SEC) / QTD_THREADS);
 
     pthread_mutex_destroy(&mutex);
 }
